abfe734249ae35b32e037a50b19a5426fd680dbe
TicaGit <thibaud.trinca@hotmail.fr>
2021-04-22 08:28:22 +0200

    add de 3 define dans capteur proxi

--- Mini_projet_base_TP5/capteur_proxi.h
+++ Mini_projet_base_TP5/capteur_proxi.h
@@ -1,6 +1,10 @@
 #ifndef CAPTEUR_PROXI_H
 #define CAPTEUR_PROXI_H
 
+#define PAS_OBSTACLE 0 //necessaire ?
+#define OBSTACLE_AVANT 1
+#define OBSTACLE_ARRIERE 2
+
 
 void capteur_proxi_start(void);
 
