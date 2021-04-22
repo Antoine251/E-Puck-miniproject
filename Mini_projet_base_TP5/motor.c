138721553e55b38286c459fdc258dd822a164859
TicaGit <thibaud.trinca@hotmail.fr>
2021-04-22 11:45:10 +0200

    add gestion des cas obstacle et avancer dans motor.c

--- Mini_projet_base_TP5/motor.c
+++ Mini_projet_base_TP5/motor.c
@@ -5,6 +5,8 @@
 #include <motors.h>
 #include <mailboxe.h>
 
+#include <capteur_proxi.h> //include pour avoir les define des etat du proximity
+
 static THD_WORKING_AREA(motor_thd_wa, 256);
 static THD_FUNCTION(motor_thd, arg){
 
@@ -28,7 +30,36 @@ static THD_FUNCTION(motor_thd, arg){
     	chSysUnlock();
     	chprintf((BaseSequentialStream *)&SDU1, "etat obstacle = %d \n", message_received_proximity);
 
-    	chThdSleepMilliseconds(50);
+    	int16_t rotation_speed_right = message_received_motor_right;
+    	int16_t rotation_speed_left = message_received_motor_left;
+    	uint8_t etat_obstacle = message_received_proximity;
+
+    	int16_t vit_moy = rotation_speed_left/2 + rotation_speed_right/2;
+
+    	switch(etat_obstacle){
+    	case PAS_OBSTACLE :
+    		left_motor_set_speed(rotation_speed_left);
+    		right_motor_set_speed(rotation_speed_right);
+    		break;
+
+    	case OBSTACLE_AVANT :
+    		left_motor_set_speed(rotation_speed_left - vit_moy);
+    		right_motor_set_speed(rotation_speed_right - vit_moy);
+    		break;
+
+    	case OBSTACLE_ARRIERE :
+    		if(vit_moy < 0){
+    			left_motor_set_speed(rotation_speed_left - vit_moy);
+    			right_motor_set_speed(rotation_speed_right - vit_moy);
+    		}else{
+    			left_motor_set_speed(rotation_speed_left);
+    			right_motor_set_speed(rotation_speed_right);
+    		}
+    		break;
+    	}
+
+
+    	chThdSleepMilliseconds(50); //20x par seconde
     }
 }
 
