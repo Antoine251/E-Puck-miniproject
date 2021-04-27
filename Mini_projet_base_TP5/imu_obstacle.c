1b77fb3950640b11a8a87343173a869259f1d106
Antoine251 <Tatane@3.0@>
2021-04-27 10:27:23 +0200

    mesure offset accélération en z

--- Mini_projet_base_TP5/imu_obstacle.c
+++ Mini_projet_base_TP5/imu_obstacle.c
@@ -5,7 +5,10 @@
 #include <sensors/imu.h>
 
 #define Z_AXIS 2
-#define NUMBER_SAMPLE_IMU 10
+#define NUMBER_SAMPLE_IMU 			100
+#define NBR_SAMPLE_IMU_CALIBRATION 	255
+
+static int16_t offset_acc_z = 0;
 
 static THD_WORKING_AREA(obs_thd_wa, 256); //256 ou moins ?
 static THD_FUNCTION(obs_thd, arg){
@@ -17,7 +20,7 @@ static THD_FUNCTION(obs_thd, arg){
 
 		int16_t z_acc = 0;
 
-		get_acc_filtered(Z_AXIS, NUMBER_SAMPLE_IMU);
+		z_acc = get_acc_filtered(Z_AXIS, NUMBER_SAMPLE_IMU) - offset_acc_z;
 
 		chprintf((BaseSequentialStream *)&SDU1, "imu values z_axis : %d \n", z_acc);
 
@@ -32,5 +35,6 @@ void imu_init(void){ //ordre ?
 	imu_start();
 	chThdCreateStatic(obs_thd_wa, sizeof(obs_thd_wa), NORMALPRIO, obs_thd, NULL);
 	calibrate_acc();
+	offset_acc_z = get_acc_filtered(Z_AXIS, NBR_SAMPLE_IMU_CALIBRATION);
 }
 
