fbfe0c568ff4a124fe278441819ac559e0254fbb
TicaGit <thibaud.trinca@hotmail.fr>
2021-04-29 10:52:44 +0200

    changements a motor

--- Mini_projet_base_TP5/motor.c
+++ Mini_projet_base_TP5/motor.c
@@ -9,10 +9,14 @@
 #include <capteur_proxi.h> 	//include pour avoir les #define de etat_obstacle
 #include <imu_obstacle.h>	//include pour avoir les #define de etat_penche
 
-#define ON_A_BUMP		1
-#define NOT_ON_A_BUMP	0
-#define DO_A_TURN		1
-#define DONT_DO_A_TURN	0
+#define ON_A_BUMP			1
+#define NOT_ON_A_BUMP		0
+#define DO_A_TURN			1
+#define DONT_DO_A_TURN		0
+#define TURN_SPEED			500 //in step/sec
+#define MAX_COUNTER_STEP 	52  // (perimetre_wheel/vitesse_tour)/(temps de la tread) = (16.8/(500/1000*13))/0.05
+
+
 
 /*********************PROTOTYPES OF INTERNAL FUNCTIONS****************************/
 void do_a_roll(void);
@@ -62,8 +66,8 @@ static THD_FUNCTION(motor_thd, arg){
     	int16_t vit_moy = rotation_speed_left/2 + rotation_speed_right/2;
 
     	//code pour detecter la fin d'une bosse
-    	uint8_t sur_une_bosse = NOT_ON_A_BUMP;
-    	uint8_t faire_un_tour = DONT_DO_A_TURN;
+    	static uint8_t sur_une_bosse = NOT_ON_A_BUMP;
+    	static uint8_t faire_un_tour = DONT_DO_A_TURN;
     	if(etat_penche == BUMP_DETECTED && sur_une_bosse == NOT_ON_A_BUMP){
     		sur_une_bosse = ON_A_BUMP;
     	}
@@ -73,35 +77,43 @@ static THD_FUNCTION(motor_thd, arg){
     	}
 
     	//obstacle a la prio sur bosse, donc tant qu'on a un obstacle, le robot ne fais pas de tour sur luimeme
+    	//mais une fois le tour lancé, il a la prio sur tout
 
+    	static int turn_counter = 0;
 
-    	switch(etat_obstacle){
-    	case PAS_OBSTACLE : 	//avance suivant les instruction de l'utilisateur, SAUF si il y'a une bosse
-    		if(faire_un_tour == DONT_DO_A_TURN){
-    			left_motor_set_speed(rotation_speed_left);
-    			right_motor_set_speed(rotation_speed_right);
-    		}else{
+    	if(faire_un_tour == DONT_DO_A_TURN){
+			switch(etat_obstacle){
+			case PAS_OBSTACLE : 	//avance suivant les instruction de l'utilisateur
+					left_motor_set_speed(rotation_speed_left);
+					right_motor_set_speed(rotation_speed_right);
+				break;
+
+			case OBSTACLE_AVANT : 	//autorise que la rotation sur place
+				left_motor_set_speed(rotation_speed_left - vit_moy);
+				right_motor_set_speed(rotation_speed_right - vit_moy);
+				break;
+
+			case OBSTACLE_ARRIERE : //bloque la direction en arriï¿½re
+				if(vit_moy < 0){
+					left_motor_set_speed(rotation_speed_left - vit_moy);
+					right_motor_set_speed(rotation_speed_right - vit_moy);
+				}else{
+					left_motor_set_speed(rotation_speed_left);
+					right_motor_set_speed(rotation_speed_right);
+				}
+				break;
+			}
+    	}else{
+    		//en train de faire le tour
+    		left_motor_set_speed(TURN_SPEED);
+    		right_motor_set_speed(TURN_SPEED);
+    		turn_counter++;
+    		if(turn_counter == MAX_COUNTER_STEP){
+    			turn_counter = 0;
     			faire_un_tour = DONT_DO_A_TURN;
-    			do_a_roll(); // comment bloquer le reste ? et quoi bloquer ?
-    			//soit bloquer tout et faire un tour
-    			//soit variable static qui compte l'anlgle fais, et on update a chaque fois quon rentre dans le thread -> garde le controle des obstacle
-    		}
-    		break;
 
-    	case OBSTACLE_AVANT : 	//autorise que la rotation sur place
-    		left_motor_set_speed(rotation_speed_left - vit_moy);
-    		right_motor_set_speed(rotation_speed_right - vit_moy);
-    		break;
-
-    	case OBSTACLE_ARRIERE : //bloque la direction en arriï¿½re
-    		if(vit_moy < 0){
-    			left_motor_set_speed(rotation_speed_left - vit_moy);
-    			right_motor_set_speed(rotation_speed_right - vit_moy);
-    		}else{
-    			left_motor_set_speed(rotation_speed_left);
-    			right_motor_set_speed(rotation_speed_right);
     		}
-    		break;
+
     	}
 
 
